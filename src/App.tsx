51b7d40e6b91a87649e354b5679c2661dd7f8610
SviatikTyshchyk <tyshchyksviatoslav@gmail.com>
2024-08-21 13:34:26 +0300

    init: add clean react_ts_tailwind project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+    return (<h1>QUIZ</h1>);
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // Listen on all available IP addresses
+    port: 3000, // Change port if needed
+  },
+})
